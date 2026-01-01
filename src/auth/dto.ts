4a7afffd16693a23ef4f32a42c9ed7d5bff7d076
Surya <suryajarajapu@gmail.com>
2026-01-01 23:55:07 +0530

    dto.ts changes

--- src/auth/dto.ts
+++ src/auth/dto.ts
@@ -1,11 +1,44 @@
+// export class RegisterDto {
+//   name!: string;
+//   email!: string;
+//   password!: string;
+//   // optional: allow role assignment for testing; in production restrict this
+//   role?: 'ADMIN' | 'SUPERVISOR' | 'STAFF';
+// }
+// export class LoginDto {
+//   email!: string;
+//   password!: string;
+// }
+
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsIn,
+} from 'class-validator';
+
 export class RegisterDto {
-  name!: string;
-  email!: string;
-  password!: string;
-  // optional: allow role assignment for testing; in production restrict this
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  password: string;
+
+  @IsOptional()
+  @IsIn(['ADMIN', 'SUPERVISOR', 'STAFF'])
   role?: 'ADMIN' | 'SUPERVISOR' | 'STAFF';
 }
+
 export class LoginDto {
-  email!: string;
-  password!: string;
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  password: string;
 }
